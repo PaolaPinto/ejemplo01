a37bbf9e038d0fea23b8dba04d6be71c0c49c408
paola pinto <71159567.pp@gmail.com>
2020-11-19 15:08:18 -0400

    datos listos

---
+++ .cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "nombre:Paola Andrea Pinto Mamani "<<endl;
+	cout << "Carrera:Ingenieria de Sistemas"<<endl;
+	system("pause");
+	return 0;
+}
